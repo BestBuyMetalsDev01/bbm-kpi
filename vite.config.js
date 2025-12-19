a7e89deca87707e750974a017bcde75874358d44
BestBuyMetalsDev01 <jacob@bestbuymetals.com>
2025-12-19 09:30:01 -0500

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bbm-kpi/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
